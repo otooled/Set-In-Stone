944b3c9a9771821054da74f323908b432a0668f2
otooled <s00003408@mail.itsligo.ie>
2015-04-03 18:16:44 +0100

    Some tidying

--- SetInStone/CalcClasses/CutCost.cs
+++ SetInStone/CalcClasses/CutCost.cs
@@ -14,48 +14,41 @@ namespace SetInStone.CalcClasses
         }
 
 
-       public static void CalcStraight(string sType, float slWidth, float slHeight, float slLength)
-        {
+       public static float CalcStraightCuts(string sType, float slWidth, float slHeight, float slLength)
+       {
+           float normalCuts = 0;
+           float surfaceArea = 0;
+           float sideArea = 0;
+
+           surfaceArea = slLength*slWidth;
+           sideArea = (slLength*slWidth)*2;
+
             List<Slab> slabs = db.Slabs.ToList();
 
             var heightOk = slabs.Where(a => a.Thickness > slHeight);
             var pickSlab = heightOk.OrderBy(a => a.Thickness).Take(1);
 
-           
+
 
         }
+
         public static float CalcCost(string sType, float slWidth, float slHeight, float slLength)
         {
             float cost = 0;
 
             var volume = slHeight*slWidth*slLength;
 
-            var stoneCost = db.Stones.Where(a => a.StoneType == sType).FirstOrDefault();//Stone.Where(a => a. > slHeight);
+            var stoneCost = db.Stones.Where(a => a.StoneType == sType).FirstOrDefault();
             cost = volume*(float)stoneCost.CostPerCube;
 
             return cost;
         }
+
         ////Pyramid surface area - formula A = lw+l.√(w2/2)²+h²+w.√(l/2)²+h²
-
-        ////step one of formula (L)(W)+(L)
-        //decimal fPart1 = customerSlabArea + slabLength;
-
-        ////step two of formula (w/2)²+h²+w - not getting the square root yet
-        //decimal fPart2 = (slabWidth / 2) * (slabWidth / 2) + (pyramidHeight * pyramidHeight) + slabWidth;
-
-        ////step three of formula (l/2)²+h² - not getting the square root yet
-
-        //decimal fPart3 = (slabLength/2)*(slabLength/2) + (pyramidHeight*pyramidHeight);
-
-        ////step four of formula - get square root of part 2 and part 3
-
-        //decimal sqrtOfPart2 = (Decimal)Math.Sqrt(Convert.ToDouble(fPart2));
-        //decimal sqrtOfPart3 = (Decimal)Math.Sqrt(Convert.ToDouble(fPart3));
-
         //decimal surfaceAreaOfPyramid = fPart1*sqrtOfPart2*sqrtOfPart3;
         public static float PyramidCutCost(string sType, float slWidth, float slHeight, float pyrHeight, float slLength)
         {
-            float pyramidCost;
+            float totalPyramidCost = 0;
 
             float baseArea = slLength*slWidth;
             ////step one of formula (L)(W)+(L)
@@ -73,7 +66,11 @@ namespace SetInStone.CalcClasses
             float sqrtOfPart2 = (float) Math.Sqrt(Convert.ToDouble(fPart2));
             float sqrtOfPart3 = (float) Math.Sqrt(Convert.ToDouble(fPart3));
             var surfaceArea = fPart1 * sqrtOfPart2 * sqrtOfPart3;
-            return surfaceArea;
+
+
+            var pyramidCost = db.Stones.Where(a => a.StoneType == sType).FirstOrDefault();
+            totalPyramidCost = surfaceArea*(float) pyramidCost.CutCost;
+            return totalPyramidCost;
         }
     }
 }
